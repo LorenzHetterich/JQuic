4ce759cd00480bd74a2914824ce6e86c4e82ed47
Lorenz Hetterich <s8lohett@stud.uni-saarland.de>
2021-04-02 18:24:37 +0200

    updated to lsquic 2.29.6 and fixed usercode integration

--- native/shared.sh
+++ native/shared.sh
@@ -1,3 +1,4 @@
 gcc -c -Wall -Werror -fpic src/*.c -I src/lsquic -I src -I/lib/jvm/java-14-openjdk-amd64/include -I/lib/jvm/java-14-openjdk-amd64/include/linux -Llibs -lpthread -lssl -ldecrepit -lcrypto -llsquic -lm
 gcc -shared -o libs/libusercode.so *.o -Llibs -lpthread -lssl -ldecrepit -lcrypto -llsquic -lm
+cp libs/libusercode.so ../src/main/resources/linux-x86-64/libusercode.so
 rm *.o
--- native/shared_windows.bat
+++ native/shared_windows.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 gcc -c -fpic -I src -I src/lsquic  src/*.c -Llibs -lssl -ldecrepit -lcrypto -llsquic -lm -lkernel32
 gcc -shared -o libs/usercode.dll *.o  -Llibs -lssl -ldecrepit -lcrypto -llsquic -lm -lkernel32
-REM rm *.o
+copy /Y libs/usercode.dll ../src/main/resources/win32-x86-64/usercode.dll
+remove *.o
